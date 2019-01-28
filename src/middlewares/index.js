dd21a7b16d1a756022e3b6d2801c3ce85cb81112
Constantine Sharow <omiceron@gmail.com>
2019-01-28 23:57:51 +0500

    Update app

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,5 +1,8 @@
 import favicon from './favicon'
 import serve from './static'
 import logger from './logger'
+import errors from './errors'
+import bodyParser from './bodyParser'
+import mongooseSession from './mongooseSession'
 
-export default [favicon, serve, logger]
+export default [favicon, serve, logger, errors, mongooseSession, bodyParser]
--- src/server.js
+++ src/server.js
@@ -1,11 +1,31 @@
 import Koa from 'koa'
 import routes from './routes'
 import middlewares from './middlewares'
+import mongoose from './libs/mongoose'
+import Element from './libs/element'
+
+import Router from 'koa-router'
+
+const newRoutes = new Router({
+  prefix: '/f',
+})
+
+newRoutes
+  .param('some', async (data, ctx, next) => {
+    if (!mongoose.Types.ObjectId.isValid(data)) return
+    ctx.some = 'new data: ' + data
+    ctx.element = Element.findById(data)
+    await next()
+  })
+  .get('/:some', async (ctx) => {
+    ctx.body = ctx.element.toObject()
+  })
 
 const app = new Koa()
 
 middlewares.forEach(m => app.use(m))
 
 app.use(routes)
+app.use(newRoutes.routes())
 
 export default app
